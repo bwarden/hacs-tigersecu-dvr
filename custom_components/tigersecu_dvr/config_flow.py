38aa29bdb3568421c764ac6325197e7f0a4cc157
Brett T. Warden <brett.warden@gmail.com>
2025-12-02 15:25:09 -0800

    Add config flow and strings

---
+++ custom_components/tigersecu_dvr/config_flow.py
@@ -0,0 +1,62 @@
+"""Config flow for Tigersecu DVR integration."""
+import logging
+
+import aiohttp
+import voluptuous as vol
+
+from homeassistant import config_entries
+from homeassistant.const import CONF_HOST, CONF_PASSWORD, CONF_USERNAME
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+
+from .api import TigersecuDVRAPI
+from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
+
+DATA_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_HOST): str,
+        vol.Required(CONF_USERNAME): str,
+        vol.Required(CONF_PASSWORD): str,
+    }
+)
+
+
+async def validate_input(hass: HomeAssistant, data: dict) -> None:
+    """Validate the user input allows us to connect."""
+    session = async_get_clientsession(hass)
+    api = TigersecuDVRAPI(
+        host=data[CONF_HOST],
+        username=data[CONF_USERNAME],
+        password=data[CONF_PASSWORD],
+        session=session,
+    )
+    await api.async_connect()
+    await api.async_disconnect()
+
+
+class TigersecuDVRConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for Tigersecu DVR."""
+
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        """Handle the initial step."""
+        errors = {}
+        if user_input is not None:
+            try:
+                await validate_input(self.hass, user_input)
+            except (aiohttp.ClientError, ConnectionError):
+                errors["base"] = "cannot_connect"
+            except Exception:  # pylint: disable=broad-except
+                _LOGGER.exception("Unexpected exception")
+                errors["base"] = "unknown"
+            else:
+                return self.async_create_entry(
+                    title=user_input[CONF_HOST], data=user_input
+                )
+
+        return self.async_show_form(
+            step_id="user", data_schema=DATA_SCHEMA, errors=errors
+        )
